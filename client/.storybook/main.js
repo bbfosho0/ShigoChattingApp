daef49808fe83c0e1d27bc1b1c41bf81858ebe8b
Yoshi Rafael Gomez Gutierrez <32444740+bbfosho0@users.noreply.github.com>
2026-08-17 18:01:42 -0500

    chore: add minimal CRA Storybook configuration

---
+++ client/.storybook/main.js
@@ -0,0 +1,18 @@
+const config = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y"
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  staticDirs: ["../public"]
+};
+
+export default config;
