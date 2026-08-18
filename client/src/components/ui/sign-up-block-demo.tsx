049fa724beca90dc9f0ceb4b501c5741566b4874
Yoshi Rafael Gomez Gutierrez <32444740+bbfosho0@users.noreply.github.com>
2026-08-17 20:22:39 -0500

    fix: remove duplicate sign up demo framing

--- client/src/components/ui/sign-up-block-demo.tsx
+++ client/src/components/ui/sign-up-block-demo.tsx
@@ -1,9 +1,5 @@
 import SignUpBlock from "components/ui/sign-up-block";
 
 export default function SignUpBlockDemo() {
-  return (
-    <div className="rounded-2xl bg-background p-2 text-foreground">
-      <SignUpBlock />
-    </div>
-  );
+  return <SignUpBlock />;
 }
