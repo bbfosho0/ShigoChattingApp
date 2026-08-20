3b85ca6191795f87760cf9258340546fcbc48472
Yoshi Rafael Gomez Gutierrez <32444740+bbfosho0@users.noreply.github.com>
2026-08-20 14:11:59 -0500

    feat: add authentication version to users

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    authVersion: { type: Number, default: 0, min: 0 },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
-// This code defines a Mongoose schema for a User model in a MongoDB database.
-// The schema includes fields for username, email, and password, all of which are required.
